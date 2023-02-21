4469fee5b989db904a588bbbf77eb9deb70dddc6
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-21 20:58:30 +0300

    r

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,21 +1,22 @@
 import express from 'express';
 const router = express.Router();
 
-router.get("/", async (req, res)=>{
 
-  res.render("index", {user:req.session.user})
-})
 router.get("/userEvent", async (req, res)=>{
 
   if(!req.session.user)
     res.sendStatus(404)
   res.redirect("/userEvent/params")
 })
-router.get("/userEvent", async (req, res)=>{
+router.get("/userEvent/params", async (req, res)=>{
 
   if(!req.session.user)
     res.sendStatus(404)
   res.render("eventElems/params.pug")
+})
+router.get("/", async (req, res)=>{
+
+  res.render("index", {user:req.session.user})
 })
 
 
