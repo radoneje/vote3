845ed13d25ead01f5a5cf07b426a6ac63cdf46ff
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-21 18:53:36 +0300

    r

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -20,11 +20,11 @@ router.post("/checkCode", async (req, res) => {
     try {
         let users = await req.knex("t_users").where({email: req.body.email, confirmCode: req.body.code})
         if (users.length == 0)
-            return req.status(404)
+            return res.status(404)
         req.session.user = users[0];
         res.json({guid: users[0].guid})
     } catch (e) {
-        req.status(404)
+        res.status(404)
     }
 })
 
