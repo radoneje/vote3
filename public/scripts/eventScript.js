b30c9a54df075bf99b704358731065b534d0d8fb
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-21 22:04:24 +0300

    r

--- public/scripts/eventScript.js
+++ public/scripts/eventScript.js
@@ -1,1 +1,11 @@
-
+document.querySelectorAll(".linkGroup").forEach(elem => {
+    elem.addEventListener("click", async () => {
+        await navigator.clipboard.writeText(elem.querySelector(".linkText").innerHTML)
+        let btn = elem.querySelector(".linkCopy")
+        let txt = btn.innerHTML
+        btn.innerHTML = "Готово"
+        setTimeout(() => {
+            btn.innerHTML = txt
+        }, 2000)
+    })
+})
