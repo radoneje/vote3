e4680892090999ef715342112fb70f93e1e77de8
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-21 22:54:48 +0300

    r

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -20,6 +20,16 @@ router.get("/userEvent/links", async (req, res)=>{
 
   res.render("eventElems/links.pug", {event:events[0]})
 })
+router.get("/userEvent/q", async (req, res)=>{
+    if(!req.session.user)
+        res.sendStatus(404)
+
+    let events=await req.knex("t_events").where({userid:req.session.user.id})
+    if(events.length==0)
+        events=await req.knex("t_events").insert({userid:req.session.user.id},"*")
+
+    res.render("eventElems/q.pug", {event:events[0]})
+})
 
 
 router.get("/qrcode/", async (req, res)=>{
