3074b999909668c2918a94f09f4e1d8849e728bc
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-21 23:34:47 +0300

    r

--- public/scripts/eventScript.js
+++ public/scripts/eventScript.js
@@ -24,6 +24,9 @@ document.querySelectorAll(".linkQr").forEach(elem => {
 })
 let qEnable=document.getElementById("qEnable")
 if(qEnable){
-    qEnable.classList.toggle("active")
+    qEnable.addEventListener("click",()=>{
+        qEnable.classList.toggle("active")
+    })
+
 }
 
