5a7773191279d27f5636be9fad515681154b5f92
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-21 17:38:25 +0300

    r

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,11 +1,19 @@
 import express from 'express';
 const router = express.Router();
 
+const  randomIntFromInterval=(min, max)=> { // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 router.get("/", async (req, res)=>{
   res.json([])
 })
 router.post("/login", async (req, res)=>{
-  res.json({email:req.body.email})
+  let users=await req.knex("t_users").where({email:req.body.email})
+  if(users.length==0)
+    users=await req.knex("t_users").insert({email:req.body.email}, "*")
+  await req.knex("t_users").update({confirmCode:randomIntFromInterval(1000,9999)})
+  res.json({email:users[0].email})
 })
 
 
