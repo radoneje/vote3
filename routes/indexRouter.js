0189c7b3e876ff00902205dece55307ea5c883f0
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-21 19:24:38 +0300

    r

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get("/", async (req, res)=>{
-  res.render("index")
+  res.render("index", {user:req.session.user})
 })
 
 export default router
