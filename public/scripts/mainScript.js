6fe3702fcd6c73c795fd34e908a8d5ee67022d15
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-21 18:41:11 +0300

    r

--- public/scripts/mainScript.js
+++ public/scripts/mainScript.js
@@ -5,19 +5,42 @@ const validateEmail = (email) => {
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       );
   };
-
+const activateCodeForm=(email){
+    let elem = document.querySelector("#regBoxCode")
+    elem.addEventListener("change", () => {
+        elem.parentNode.classList.remove("error")
+        if(!elem.value.length)
+            elem.parentNode.classList.add("error")
+        return;
+    })
+    let btn=document.querySelector("#regBoxConfirmButton")
+    btn.addEventListener("click", async () => {
+        let responce = await fetch("/api/checkCode/", {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json;charset=utf-8'},
+            body: JSON.stringify({email, code:elem.value})
+        })
+    });
+}
 
 try {
     let email = localStorage.getItem("email")
     let elem = document.querySelector("#regBoxEmail")
     if (elem) {
         elem.value = email;
-        elem.addEventListener("change", () => {
+        elem.addEventListener("change", async () => {
             elem.parentNode.classList.remove("error")
             if(validateEmail(elem.value))
                 localStorage.setItem("email", elem.value)
             else
                 elem.parentNode.classList.add("error")
+            if (responce.ok) {
+                let result = await responce.json();
+                document.location.href="/userEvent/"+result.guid;
+            }
+            else
+                elem.parentNode.classList.add("error")
+
         })
     }
     let btn=document.querySelector("#regBoxButton")
@@ -38,6 +61,7 @@ try {
                 })
                 let code=document.querySelector("#regBoxCode")
                 code.focus();
+                activateCodeFortm(elem.value);
             }
         })
     }
