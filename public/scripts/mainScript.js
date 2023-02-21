b79694beae7490eb5d049136111f965cf70f026e
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-21 18:44:58 +0300

    r

--- public/scripts/mainScript.js
+++ public/scripts/mainScript.js
@@ -5,7 +5,7 @@ const validateEmail = (email) => {
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       );
   };
-const activateCodeForm=(email){
+const activateCodeForm=(email)=>{
     let elem = document.querySelector("#regBoxCode")
     elem.addEventListener("change", () => {
         elem.parentNode.classList.remove("error")
