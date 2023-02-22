05d70095fb3b8a6d6572a3611071b5fc12bd5e3b
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-22 09:55:11 +0300

    r

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -13,8 +13,6 @@ router.get("/userEvent", async (req, res)=>{
 router.get("/userEvent/links", async (req, res)=>{
   if(!req.session.user)
     res.sendStatus(404)
-    console.log(req)
-
   let events=await req.knex("t_events").where({userid:req.session.user.id})
   if(events.length==0)
     events=await req.knex("t_events").insert({userid:req.session.user.id},"*")
