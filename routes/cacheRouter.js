b87b381b451727ba0051ca7b07019e45db9a4d84
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-22 10:12:16 +0300

    r

--- routes/cacheRouter.js
+++ routes/cacheRouter.js
@@ -17,7 +17,7 @@ const __dirname = path.dirname(__filename);
 
 router.get("/u/:short", async (req, res) => {
    try {
-       let events = await req.knex("t_evants").where({short: req.params.short, isDeleted: false})
+       let events = await req.knex("t_events").where({short: req.params.short, isDeleted: false})
        if (events.length == 0)
            return res.sendStatus(404);
        res.json({a: "ok"})
