b3fe8e504b2532df42d3d60683976a08e77375e9
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-22 15:16:16 +0300

    r

--- public/scripts/workScript.js
+++ public/scripts/workScript.js
@@ -13,7 +13,7 @@ let app = new Vue({
     },
     methods:{
         onScrollQ:function(e){
-            console.log("onScrollQ",e)
+            console.log("onScrollQ",e.target.scrollTop,e.target.scrollY )
         },
         regPerson: async function(){
             if(!this.person.i || this.person.i.length<0){
