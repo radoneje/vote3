34406d019fbcc3b7a3530f26153d158b58422950
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-22 10:51:02 +0300

    r

---
+++ public/scripts/workScript.js
@@ -0,0 +1,11 @@
+let app = new Vue({
+    el:"#app",
+    data:{
+        short:short,
+    },
+    methods:{},
+    watch:{},
+    mounted:async function (){
+        console.log("vie mounted")
+    }
+})
