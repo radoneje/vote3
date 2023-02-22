c40626d787719ad23dd4dcfc9533cf1c6f0b0ce8
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-22 10:07:21 +0300

    r

--- routes/cacheRouter.js
+++ routes/cacheRouter.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-const router2 = express.Router();
+const router = express.Router();
 import nodemailer from  'nodemailer'
 import path from 'path';
 import fs from 'fs';
@@ -12,26 +12,22 @@ const config= require('../config.json');
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-let router={
-    get:(url,params)=>{
-        try {
-            router2.get(url, params)
-        }
-        catch (e){
-            params.res.sendStatus(500);
-            comsole.log(e)
-        }
-    }
-}
+
 
 
 router.get("/u/:short", async (req, res) => {
-   let events=await req.knex("t_evants").where({short:req.params.short, isDeleted:false})
-    if(events.length==0)
-        return res.sendStatus(404);
-    res.json({a:"ok"})
+   try {
+       let events = await req.knex("t_evants").where({short: req.params.short, isDeleted: false})
+       if (events.length == 0)
+           return res.sendStatus(404);
+       res.json({a: "ok"})
+   }
+   catch (e){
+       console.warn(e)
+       res.text(e).sendStatus(500)
+   }
 })
 
 
 
-export default router2
+export default router
