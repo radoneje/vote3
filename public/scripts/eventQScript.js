12e1aa2519ee0b2b2aed3eb499c71467caa70790
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-23 00:06:29 +0300

    r

--- public/scripts/eventQScript.js
+++ public/scripts/eventQScript.js
@@ -143,6 +143,20 @@ if(dropArea){
         e.preventDefault()
         e.stopPropagation()
     }
+    ['dragenter', 'dragover'].forEach(eventName => {
+        dropArea.addEventListener(eventName, highlight, false)
+    })
+
+    ;['dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, unhighlight, false)
+    })
+
+    function highlight(e) {
+        dropArea.classList.add('highlight')
+    }
+    function unhighlight(e) {
+        dropArea.classList.remove('highlight')
+    }
 }
 
 
