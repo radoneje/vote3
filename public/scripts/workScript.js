e970db46f87d8ffa14a8796c60df3f718deaa549
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-22 15:00:14 +0300

    r

--- public/scripts/workScript.js
+++ public/scripts/workScript.js
@@ -166,14 +166,15 @@ const post =(url, body)=>{
 
     })
 
-    function isInViewport(element) {
-        const rect = element.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    }
 
+
+}
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
 }
