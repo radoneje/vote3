cce9dc938aba2a6018e5b9e39558c06745139099
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-22 10:59:10 +0300

    r

--- public/scripts/workScript.js
+++ public/scripts/workScript.js
@@ -6,7 +6,7 @@ let app = new Vue({
     methods:{
         updateStatus:async function(lastTime){
             let timeout=20;
-            let res=await fetch("/c/status/"+short)
+            let res=await fetch("/c/status/"+short+"/"+lastTime)
             if(res.ok){
                 let r=await res.json();
                 timeout=r.timeout;
