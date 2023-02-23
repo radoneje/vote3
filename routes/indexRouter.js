c38bf459cc32af3993f6896d5cd53c4b641495eb
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-23 20:07:54 +0300

    r

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -43,6 +43,16 @@ router.get("/userEvent/files", async (req, res)=>{
 
     res.render("eventElems/files.pug", {event:events[0]})
 })
+router.get("/userEvent/votes", async (req, res)=>{
+    if(!req.session.user)
+        res.sendStatus(404)
+
+    let events=await req.knex("t_events").where({userid:req.session.user.id})
+    if(events.length==0)
+        events=await req.knex("t_events").insert({userid:req.session.user.id},"*")
+
+    res.render("eventElems/files.pug", {event:events[0]})
+})
 
 
 
