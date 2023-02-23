589dc746f3b5a48bd431200cbb7710271b9415ba
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-23 22:42:54 +0300

    r

--- public/scripts/eventQScript.js
+++ public/scripts/eventQScript.js
@@ -205,7 +205,7 @@ let app = new Vue({
                             })
                         }
                     })
-                    this.files=this.files.filter(qq=>!qq.isDeleted)
+                    this.votes=this.votes.filter(qq=>!qq.isDeleted)
                 }
 
                 /////
