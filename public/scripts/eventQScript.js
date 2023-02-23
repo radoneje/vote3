ed979421350a2898f5c150028687114e7f106132
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-23 23:59:30 +0300

    r

--- public/scripts/eventQScript.js
+++ public/scripts/eventQScript.js
@@ -1,61 +1,59 @@
-
-
 let app = new Vue({
-    el:"#app",
-    data:{
-        event:event,
-        q:[],
-        files:[],
-        uploading:[],
-        votes:[]
+    el: "#app",
+    data: {
+        event: event,
+        q: [],
+        files: [],
+        uploading: [],
+        votes: []
     },
-    methods:{
-        addAnswer:async function (item){
-            let r=await post("/api/addAnswer",{id:item.id})
-            if(r.err)
+    methods: {
+        addAnswer: async function (item) {
+            let r = await post("/api/addAnswer", {id: item.id})
+            if (r.err)
                 return console.warn(r.message)
             item.answers.push(r.data)
         },
-        changeAnswer:async function (col,item){
-            let prm={};
-            prm[col]=item[col]
-            let r=await post("/api/changeAnswer",{id:item.id,prm})
+        changeAnswer: async function (col, item) {
+            let prm = {};
+            prm[col] = item[col]
+            let r = await post("/api/changeAnswer", {id: item.id, prm})
         },
-        changeVote:async function (col,item){
-           let prm={};
-            prm[col]=item[col]
-            let r=await post("/api/changeVote",{id:item.id,prm})
+        changeVote: async function (col, item) {
+            let prm = {};
+            prm[col] = item[col]
+            let r = await post("/api/changeVote", {id: item.id, prm})
         },
 
-        addVote:async function (item){
-            let r=await post("/api/addVote",{short:event.short})
-            if(r.err)
+        addVote: async function (item) {
+            let r = await post("/api/addVote", {short: event.short})
+            if (r.err)
                 return console.warn(e.message)
             this.votes.push(r.data);
         },
-        downloadEventFile:function (item){
-            let a=document.createElement("a")
-            a.href="/file/"+item.short
-            a.download=item.originalname;
+        downloadEventFile: function (item) {
+            let a = document.createElement("a")
+            a.href = "/file/" + item.short
+            a.download = item.originalname;
             a.click();
         },
-        uploadFile:async function(file){
+        uploadFile: async function (file) {
 
-            let res=await get("/api/newFile")
-            if(res.err)
+            let res = await get("/api/newFile")
+            if (res.err)
                 return console.warn(res.message);
-            let fileid=res.data
-            let uploadItem={id:fileid, name:file.name, done:0, total:file.size, percent:0, status:0, err:false}
+            let fileid = res.data
+            let uploadItem = {id: fileid, name: file.name, done: 0, total: file.size, percent: 0, status: 0, err: false}
             this.uploading.push(uploadItem);
-            let fd=new FormData();
+            let fd = new FormData();
             fd.append("file", file)
             fd.append("name", file.name)
             fd.append("id", fileid)
             const xhr = new XMLHttpRequest()
             xhr.responseType = 'json'
-            xhr.onreadystatechange = async ()=> {
+            xhr.onreadystatechange = async () => {
                 if (xhr.readyState == XMLHttpRequest.DONE) {
-                    if(xhr.status == 200) {
+                    if (xhr.status == 200) {
                         let res = await post("/api/fileToEvent", {fileid, short: event.short})
                         if (!res.err) {
                             this.files.push(res.data)
@@ -64,16 +62,15 @@ let app = new Vue({
                             }, 2000)
                         } else
                             console.warn(res.message)
-                    }
-                    else {
-                        uploadItem.err=true
+                    } else {
+                        uploadItem.err = true
                     }
                 }
             }
-            xhr.upload.onprogress=(ev)=>{
-                uploadItem.done=ev.loaded
-                uploadItem.total=ev.total
-                uploadItem.percent=parseInt((parseFloat(ev.loaded)/parseFloat(ev.total))*100)+"%"
+            xhr.upload.onprogress = (ev) => {
+                uploadItem.done = ev.loaded
+                uploadItem.total = ev.total
+                uploadItem.percent = parseInt((parseFloat(ev.loaded) / parseFloat(ev.total)) * 100) + "%"
                 console.log(ev.loaded, ev.total)
             }
 
@@ -81,95 +78,104 @@ let app = new Vue({
             xhr.send(fd)
 
         },
-        selectFile:function (){
-            let inp=document.createElement("input")
-            inp.type="file";
+        selectFile: function () {
+            let inp = document.createElement("input")
+            inp.type = "file";
             inp.click();
-            inp.onchange= ()=>{
+            inp.onchange = () => {
                 this.uploadFile(inp.files[0]);
             }
         },
-        approveAllFile:async function(column, status){
-            for(let item of this.files)
-            {
-                item[column]=status;
-                let dt={id:item.id}
-                dt[column]=item[column]
-                this.files=this.files.filter(qq=>!qq.isDeleted);
-                await post("/api/file/",dt);
+        approveAllFile: async function (column, status) {
+            for (let item of this.files) {
+                item[column] = status;
+                let dt = {id: item.id}
+                dt[column] = item[column]
+                this.files = this.files.filter(qq => !qq.isDeleted);
+                await post("/api/file/", dt);
             }
         },
-        approveAllQ:async function(column, status){
-            for(let item of this.q)
-            {
-                item[column]=status;
-                let dt={id:item.id}
-                dt[column]=item[column]
-                this.q=this.q.filter(qq=>!qq.isDeleted);
-                await post("/api/q/",dt);
+        approveAllQ: async function (column, status) {
+            for (let item of this.q) {
+                item[column] = status;
+                let dt = {id: item.id}
+                dt[column] = item[column]
+                this.q = this.q.filter(qq => !qq.isDeleted);
+                await post("/api/q/", dt);
             }
         },
-        changeFile:async function( item){
-            await post("/api/file/",item);
+        changeFile: async function (item) {
+            await post("/api/file/", item);
         },
-        toogleAnswer:async function(column, item){
+        toogleAnswer: async function (column, item) {
             //if(column=="isComplite" && !item.isActive)
             //    return;
-            item[column]=!item[column]
-            let dt={id:item.id}
-            dt[column]=item[column]
-            this.votes.forEach(vote=>{
-                if(vote.id==item.voteid){
-                    vote.answers=vote.answers.filter(a=>!a.isDeleted)
+            if (column == "isDeleted") {
+                let exit = false;
+                this.votes.forEach(v => {
+                    if (v.id == item.id) {
+                        if(v.answers.filter(a=>!a.isDeleted)<=1)
+                            exit=true
+                    }
+                })
+                if(exit)
+                    return alert("Ответов должно быть больше двух, удалить невозможно.")
+            }
+            item[column] = !item[column]
+            let dt = {id: item.id}
+            dt[column] = item[column]
+            this.votes.forEach(vote => {
+                if (vote.id == item.voteid) {
+                    vote.answers = vote.answers.filter(a => !a.isDeleted)
                 }
             })
-            await post("/api/answer/",dt);
+            await post("/api/answer/", dt);
         },
-        toogleVote:async function(column, item){
+        toogleVote: async function (column, item) {
             //if(column=="isComplite" && !item.isActive)
             //    return;
-            item[column]=!item[column]
-            let dt={id:item.id}
-            dt[column]=item[column]
-            this.votes=this.votes.filter(qq=>!qq.isDeleted);
-            await post("/api/vote/",dt);
+            item[column] = !item[column]
+            let dt = {id: item.id}
+            dt[column] = item[column]
+            this.votes = this.votes.filter(qq => !qq.isDeleted);
+            await post("/api/vote/", dt);
         },
-        toogleFile:async function(column, item){
-            item[column]=!item[column]
-            let dt={id:item.id}
-            dt[column]=item[column]
-            this.files=this.files.filter(qq=>!qq.isDeleted);
-            await post("/api/file/",dt);
+        toogleFile: async function (column, item) {
+            item[column] = !item[column]
+            let dt = {id: item.id}
+            dt[column] = item[column]
+            this.files = this.files.filter(qq => !qq.isDeleted);
+            await post("/api/file/", dt);
         },
-        toogleQ:async function(column, item){
-            item[column]=!item[column]
-            let dt={id:item.id}
-            dt[column]=item[column]
-            this.q=this.q.filter(qq=>!qq.isDeleted);
-            await post("/api/q/",dt);
+        toogleQ: async function (column, item) {
+            item[column] = !item[column]
+            let dt = {id: item.id}
+            dt[column] = item[column]
+            this.q = this.q.filter(qq => !qq.isDeleted);
+            await post("/api/q/", dt);
         },
-        toogleEvent:async function(column){
-            this.event[column]=!this.event[column]
-            let dt={id:event.id}
-            dt[column]=this.event[column]
-            await post("/api/event/",dt);
+        toogleEvent: async function (column) {
+            this.event[column] = !this.event[column]
+            let dt = {id: event.id}
+            dt[column] = this.event[column]
+            await post("/api/event/", dt);
         },
-        updateStatus:async function(lastTime){
-            let timeout=20;
-            let res=await fetch("/c/status/"+event.short+"/"+lastTime+"?prm=all")
-            if(res.ok){
-                let r=await res.json();
-                timeout=r.timeout;
-                lastTime=r.lastTime;
-                if(r.event)
-                    this.event=r.event;
+        updateStatus: async function (lastTime) {
+            let timeout = 20;
+            let res = await fetch("/c/status/" + event.short + "/" + lastTime + "?prm=all")
+            if (res.ok) {
+                let r = await res.json();
+                timeout = r.timeout;
+                lastTime = r.lastTime;
+                if (r.event)
+                    this.event = r.event;
 
 
-                if(r.q){
-                    r.q.forEach(item=>{
-                        if(this.q.filter(qq=>qq.id==item.id).length==0) {
+                if (r.q) {
+                    r.q.forEach(item => {
+                        if (this.q.filter(qq => qq.id == item.id).length == 0) {
 
-                            if(this.q.length>1) {
+                            if (this.q.length > 1) {
                                 let lastElem = document.querySelector(".qItem[qid='" + this.q[this.q.length - 1].id + "']")
                                 if (lastElem && isInViewport(lastElem))
                                     setTimeout(() => {
@@ -179,49 +185,46 @@ let app = new Vue({
                                     this.newQ++;
                             }
                             this.q.push(item)
-                        }
-                        else{
-                            this.q.forEach(qq=>{
-                                if(qq.id==item.id){
-                                    qq.text=item.text;
-                                    qq.isMod=item.isMod;
-                                    qq.isDeleted=item.isDeleted;
+                        } else {
+                            this.q.forEach(qq => {
+                                if (qq.id == item.id) {
+                                    qq.text = item.text;
+                                    qq.isMod = item.isMod;
+                                    qq.isDeleted = item.isDeleted;
                                 }
                             })
                         }
                     })
-                    this.q=this.q.filter(qq=>!qq.isDeleted)
+                    this.q = this.q.filter(qq => !qq.isDeleted)
                 }
                 /////////////
-                if(r.files){
-                    r.files.forEach(item=>{
-                        if(this.files.filter(qq=>qq.id==item.id).length==0) {
+                if (r.files) {
+                    r.files.forEach(item => {
+                        if (this.files.filter(qq => qq.id == item.id).length == 0) {
 
                             this.files.push(item)
-                        }
-                        else{
-                            this.files.forEach(qq=>{
-                                if(qq.id==item.id){
-                                    qq.title=item.title;
-                                    qq.isMod=item.isMod;
-                                    qq.isDeleted=item.isDeleted;
+                        } else {
+                            this.files.forEach(qq => {
+                                if (qq.id == item.id) {
+                                    qq.title = item.title;
+                                    qq.isMod = item.isMod;
+                                    qq.isDeleted = item.isDeleted;
                                 }
                             })
                         }
                     })
-                    this.files=this.files.filter(qq=>!qq.isDeleted)
+                    this.files = this.files.filter(qq => !qq.isDeleted)
                 }
                 /////////////
-                if(r.votes){
-                    r.votes.forEach(item=>{
-                        if(this.votes.filter(qq=>qq.id==item.id).length==0) {
+                if (r.votes) {
+                    r.votes.forEach(item => {
+                        if (this.votes.filter(qq => qq.id == item.id).length == 0) {
 
                             this.votes.push(item)
-                        }
-                        else{
-                            this.votes.forEach(qq=>{
-                                if(qq.id==item.id){
-                                    qq=structuredClone(item)
+                        } else {
+                            this.votes.forEach(qq => {
+                                if (qq.id == item.id) {
+                                    qq = structuredClone(item)
                                     //qq.title=item.title;
                                     //qq.isMod=item.isMod;
                                     //qq.isDeleted=item.isDeleted;
@@ -229,34 +232,33 @@ let app = new Vue({
                             })
                         }
                     })
-                    this.votes=this.votes.filter(qq=>!qq.isDeleted)
+                    this.votes = this.votes.filter(qq => !qq.isDeleted)
                 }
 
                 /////
             }
-            setTimeout(()=>{
+            setTimeout(() => {
                 this.updateStatus(lastTime)
-            }, timeout*1000);
+            }, timeout * 1000);
         }
     },
-    watch:{
-
-    },
-    mounted:async function () {
+    watch: {},
+    mounted: async function () {
         this.updateStatus(0)
     }
 })
 
 let dropArea = document.getElementById('fileWr')
-if(dropArea){
+if (dropArea) {
     ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
         dropArea.addEventListener(eventName, preventDefaults, false)
     })
 
-    function preventDefaults (e) {
+    function preventDefaults(e) {
         e.preventDefault()
         e.stopPropagation()
     }
+
     ['dragenter', 'dragover'].forEach(eventName => {
         dropArea.addEventListener(eventName, highlight, false)
     })
@@ -268,15 +270,17 @@ if(dropArea){
     function highlight(e) {
         dropArea.classList.add('highlight')
     }
+
     function unhighlight(e) {
         dropArea.classList.remove('highlight')
     }
+
     dropArea.addEventListener('drop', handleDrop, false)
 
     async function handleDrop(e) {
         let dt = e.dataTransfer
         let files = dt.files
-        for(let file of files){
+        for (let file of files) {
             await app.uploadFile(file)
         }
     }
@@ -291,39 +295,37 @@ const validateEmail = (email) => {
         );
 };
 
-const post =(url, body)=>{
-    return new Promise(async (resolve, reject)=>{
+const post = (url, body) => {
+    return new Promise(async (resolve, reject) => {
         let res = await fetch(url, {
             method: 'POST',
             headers: {'Content-Type': 'application/json;charset=utf-8'},
             body: JSON.stringify(body)
         })
-        if(res.ok){
-            resolve({data:await res.json()})
-        }
-        else
-            resolve({err:true, message:await res.text()})
+        if (res.ok) {
+            resolve({data: await res.json()})
+        } else
+            resolve({err: true, message: await res.text()})
 
     })
 }
-const get =(url)=>{
-    return new Promise(async (resolve, reject)=>{
+const get = (url) => {
+    return new Promise(async (resolve, reject) => {
         let res = await fetch(url, {
             method: 'GET',
         })
-        if(res.ok){
-            let ret=await res.json()
+        if (res.ok) {
+            let ret = await res.json()
 
-            resolve({data:ret})
-        }
-        else
-            resolve({err:true, message:await res.text()})
+            resolve({data: ret})
+        } else
+            resolve({err: true, message: await res.text()})
 
     })
 }
-const isInViewport=(element) =>{
+const isInViewport = (element) => {
     const rect = element.getBoundingClientRect();
     const parentRect = element.parentNode.getBoundingClientRect();
-    return rect.top<parentRect.bottom
+    return rect.top < parentRect.bottom
 
 }
