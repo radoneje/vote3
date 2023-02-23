bcc898a5372cda09098287d6aa9e412af183c548
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-24 01:04:01 +0300

    r

--- public/scripts/workScript.js
+++ public/scripts/workScript.js
@@ -18,7 +18,7 @@ let app = new Vue({
             return localStorage.getItem("answer_"+answer.id)
         },
         voting:async function(answer, vote){
-            if(!answer.isMulti){
+            if(!vote.isMulti){
                 vote.answers.forEach(a=>{
                     localStorage.removeItem("answer_"+a.id)
                 })
