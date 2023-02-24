ffd3bcfa00e8ed0e1350f658f8d5b92e17ba5f65
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-24 12:08:52 +0300

    r

--- routes/cacheRouter.js
+++ routes/cacheRouter.js
@@ -71,9 +71,7 @@ router.get("/status/:short/:lastTime?", async (req, res) => {
             if(req.query.prm!="all"){
                 votes.forEach(vote=>{
                     if(!vote.isActive)
-                        delete vote.title;
-                        delete vote.answers;
-                        //vote={id:vote.id, isActive:false}
+                        vote={id:vote.id, isActive:false}
                 })
             }
             ret.votes=votes;
