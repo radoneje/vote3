3287202ae5170f2aa9d2a0589199755d23afd622
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-27 23:53:53 +0300

    r

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -102,8 +102,8 @@ router.post("/player", async (req, res) => {
     try {
         let id=req.body.id;
         delete req.body.id;
-        let r=await req.knex("t_palyers").update(req.body,"*").where({id:id});
-        r=await req.knex("v_palyers").where({id:r[0].id})
+        let r=await req.knex("t_players").update(req.body,"*").where({id:id});
+        r=await req.knex("v_players").where({id:r[0].id})
         res.json(r[0])
     } catch (e) {
         res.status(404).send(e.toString())
