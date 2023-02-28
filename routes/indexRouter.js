e99dafae6195110f2baeaef8bbb203a6237dc2ee
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-28 13:07:23 +0300

    r

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -75,6 +75,18 @@ router.get("/userEvent/player", async (req, res)=>{
 })
 
 
+router.get("/userEvent/baro", async (req, res)=>{
+    if(!req.session.user)
+        res.sendStatus(404)
+
+    let events=await req.knex("t_events").where({userid:req.session.user.id})
+    if(events.length==0)
+        events=await req.knex("t_events").insert({userid:req.session.user.id},"*")
+
+    res.render("eventElems/baro.pug", {event:events[0]})
+})
+
+
 
 
 
