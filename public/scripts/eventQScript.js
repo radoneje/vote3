f71fea1a84cade84e2bd1c489a11a3cc2ade4024
radoneje <53825282+radoneje@users.noreply.github.com>
2023-03-01 00:39:43 +0300

    r

--- public/scripts/eventQScript.js
+++ public/scripts/eventQScript.js
@@ -15,8 +15,10 @@ let app = new Vue({
             let elem=document.getElementById("baroLike"+baro.id)
             if(!elem)
                 return;
-            if(elem.classList.contains("active"))
+            if(elem.classList.contains("active")) {
+                elem.classList.remove("active")
                 return;
+            }
             elem.classList.add("active")
             if(elem.classList.contains("clicked"))
                 return;
