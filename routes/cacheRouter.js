889a03a472eb68fcc4c232bf4efbf79570a7f40c
radoneje <53825282+radoneje@users.noreply.github.com>
2023-03-02 17:55:53 +0300

    r

--- routes/cacheRouter.js
+++ routes/cacheRouter.js
@@ -57,6 +57,7 @@ router.get("/status/:short/:lastTime?", async (req, res) => {
                 title: events[0].title,
                 isQ: events[0].isQ,
                 isPlayer: events[0].isPlayer,
+                settings:events[0].settings,
             }
             if (req.query.prm == "all") {
                 ret.event.isQpreMod = events[0].isQpreMod
