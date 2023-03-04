cfc7ceb09d3a29a6e2abfd114778e85f7633ea3f
radoneje <53825282+radoneje@users.noreply.github.com>
2023-03-04 17:04:34 +0300

    r

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -138,7 +138,7 @@ router.get("/verify", async (req, res)=>{
 
     if(req.query.error)
         return res.render("index", {user:req.session.user})
-    let access_token=await(axios.get("https://oauth.vk.com/access_token/?client_id=51571826&client_secret=n7zOChnGVZv8clOYDhcx&redirect_uri=https://event-24.ru/verify"))
+    let access_token=await(axios.get("https://oauth.vk.com/access_token/?client_id=51571826&client_secret=n7zOChnGVZv8clOYDhcx&redirect_uri=https://event-24.ru/verify&code="+req.query.code))
 //verify?code=cd83a74d881f117747
     console.log(access_token)
     res.render("index", {user:req.session.user})
