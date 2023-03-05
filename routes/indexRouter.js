53358df59fcdeb38251488a5258d4a6ab997b5fb
radoneje <53825282+radoneje@users.noreply.github.com>
2023-03-05 13:40:55 +0300

    r

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -167,7 +167,7 @@ router.get("/verify", async (req, res)=>{
 
     req.session.user=users[0];
 
-    res.retirect("/")
+    res.redirect("/")
 })
 
 router.get("/", async (req, res)=>{
@@ -213,7 +213,7 @@ router.get("/verifyYandex", async (req, res)=> {
 
     req.session.user=users[0];
 
-    res.retirect("/")
+    res.redirect("/")
 
 });
 
